2c9dfcb4744837b1fb2e09008ac32d7b6f573170
andersonmarin <anderson.marin@outlook.com>
2023-05-22 09:19:20 -0300

    refactor and split

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+use std::env;
+use std::process;
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents =
-        std::fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{}", contents);
+    if let Err(err) = minigrep::run(config) {
+        println!("Application error: {}", err);
+        process::exit(1);
+    }
 }
